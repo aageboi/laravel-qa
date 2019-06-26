86f441fbb64ab5562467ba78fec3e1b73c4140ee
Heri Gunawan <heri@tirto.id>
2019-06-26 13:47:56 +0700

    Update best_answer_id on questions table when answer removed in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -47,13 +47,16 @@ class Answer extends Model
     	// });
 
     	static::deleted(function($answer) {
-    		$question = $answer->question;
-    		$question->decrement('answers_count');
+    		$answer->question->decrement('answers_count');
 
-    		if ($question->best_answer_id == $answer->id) {
-    			$question->best_answer_id = NULL;
-    			$question->save();
-    		}
+    		// cara lain untuk set null di best_answer_id jika dihapus
+    		// $question = $answer->question;
+    		// $question->decrement('answers_count');
+
+    		// if ($question->best_answer_id == $answer->id) {
+    		// 	$question->best_answer_id = NULL;
+    		// 	$question->save();
+    		// }
     	});
     }
 }
