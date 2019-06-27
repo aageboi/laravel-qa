ac11674732b7c7650184cfa14f02250f7e3dc8f3
Heri Gunawan <heri@tirto.id>
2019-06-27 08:41:08 +0700

    - Display all question in root route (front page) - sort the answer by votes count - hide the answer's container if no answer - show an alert when question record empty

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -19,7 +19,7 @@
                 <div class="card-body">
                 @include ('layouts._messages')
 
-                @foreach ($questions as $question)
+                @forelse ($questions as $question)
                 	<div class="media">
                 		<div class="d-flex flex-column counters">
                 			<div class="votes">
@@ -61,7 +61,11 @@
                 		</div>
                 	</div>
                 	<hr>
-                @endforeach
+                @empty
+                <div class="alert alert-warning">
+                    <strong>Sorry</strong>, there are no questions available
+                </div>
+                @endforelse
 
                 {{ $questions->links() }}
                 </div>
